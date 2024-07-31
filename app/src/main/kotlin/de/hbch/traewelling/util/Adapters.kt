b7b1cad4b403eff1fed00251a4390e8cd67719db
Jonas Heubuch <jonas.heubuch@outlook.com>
2024-07-31 20:54:04 +0200

    :sparkles: Add swiss product icons (#399)

--- app/src/main/kotlin/de/hbch/traewelling/util/Adapters.kt
+++ app/src/main/kotlin/de/hbch/traewelling/util/Adapters.kt
@@ -1,9 +1,21 @@
 package de.hbch.traewelling.util
 
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.text.InlineTextContent
+import androidx.compose.foundation.text.appendInlineContent
+import androidx.compose.material3.Icon
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.AnnotatedString
+import androidx.compose.ui.text.Placeholder
+import androidx.compose.ui.text.PlaceholderVerticalAlign
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.unit.sp
 import com.auth0.android.jwt.JWT
 import de.hbch.traewelling.R
 import de.hbch.traewelling.api.models.station.Station
@@ -84,14 +96,47 @@ fun getLastDestination(trip: HafasTrip): String {
     }
 }
 
-fun getSwitzerlandLineName(productName: String, lineId: String): String? {
+fun getSwitzerlandLineName(productName: String, lineId: String): Pair<AnnotatedString?, Map<String, InlineTextContent>>? {
     // Switzerland lines start with 85 in the second block of line id
-    val splitLineId = lineId.split("-")
-    if (splitLineId.getOrNull(1)?.startsWith("85") == true) {
-        val lineNumber = lineId.split("-").getOrNull(2) ?: return null
-        return "$productName ${lineNumber.uppercase()}"
+    val match = "\\w+-85\\w*-(\\w+)\$".toRegex().find(lineId)
+    if (match != null) {
+        val inlineTextContent = mutableMapOf<String, InlineTextContent>()
+        val builder = AnnotatedString.Builder()
+
+        val icon = when(productName) {
+            "IR" -> R.drawable.ic_ch_ir
+            "IC" -> R.drawable.ic_ch_ic
+            "EC" -> R.drawable.ic_ch_ec
+            else -> null
+        }
+        val italicProduct = if (productName == "PE") "PE" else null
+
+        if (icon == null && italicProduct == null)
+            return null
+
+        if (icon != null) {
+            builder.appendInlineContent("product", productName)
+            inlineTextContent["product"] = InlineTextContent(
+                Placeholder(28.sp, 12.sp, PlaceholderVerticalAlign.TextCenter)
+            ) {
+                Icon(
+                    painter = painterResource(id = icon),
+                    contentDescription = null,
+                    tint = Color.White,
+                    modifier = Modifier.fillMaxSize()
+                )
+            }
+        }
+        if (italicProduct != null) {
+            builder.pushStyle(SpanStyle(fontStyle = FontStyle.Italic))
+            builder.append(italicProduct)
+            builder.pop()
+        }
+        builder.append(" ")
+        builder.append(match.groupValues.getOrNull(1)?.uppercase() ?: "")
+        return Pair(builder.toAnnotatedString(), inlineTextContent)
     }
-    return null
+    return Pair(null, mapOf())
 }
 
 private fun clarifyRingbahnBerlin(trip: HafasTrip): String {
