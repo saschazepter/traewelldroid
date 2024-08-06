4142f830d0b30e25925659a711e996d4df226ebc
Jonas Heubuch <jonas.heubuch@outlook.com>
2024-08-06 12:04:19 +0200

    :sparkles: Display platform on departure board (#404)

--- app/src/main/kotlin/de/hbch/traewelling/ui/include/status/ActiveStatusBar.kt
+++ app/src/main/kotlin/de/hbch/traewelling/ui/include/status/ActiveStatusBar.kt
@@ -3,9 +3,7 @@ package de.hbch.traewelling.ui.include.status
 import androidx.compose.animation.core.FastOutSlowInEasing
 import androidx.compose.animation.core.animateFloatAsState
 import androidx.compose.animation.core.tween
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -23,7 +21,6 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextOverflow
@@ -31,6 +28,7 @@ import androidx.compose.ui.unit.dp
 import de.hbch.traewelling.R
 import de.hbch.traewelling.api.models.status.Status
 import de.hbch.traewelling.ui.composables.LineIcon
+import de.hbch.traewelling.ui.searchConnection.Platform
 import de.hbch.traewelling.ui.user.getDurationString
 import kotlinx.coroutines.delay
 import java.time.Duration
@@ -102,23 +100,10 @@ fun ActiveStatusBar(
                             text = stringResource(id = R.string.time_left, getDurationString(duration.absoluteValue))
                         )
                     }
-                    val platform = status.journey.destination.arrivalPlatformReal
-                        ?: status.journey.destination.arrivalPlatformPlanned
-                    if (platform != null) {
-                        Box(
-                            modifier = Modifier
-                                .background(Color.Blue)
-                                .padding(4.dp)
-                        ) {
-                            Text(
-                                text = stringResource(
-                                    id = R.string.platform,
-                                    platform
-                                ),
-                                color = Color.White
-                            )
-                        }
-                    }
+                    Platform(
+                        planned = status.journey.destination.arrivalPlatformPlanned,
+                        real = status.journey.destination.arrivalPlatformReal
+                    )
                 }
             }
             LinearProgressIndicator(
