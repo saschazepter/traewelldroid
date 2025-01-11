248a9afdc4117391e7d57e4b37e681aaae092a88
Jonas Heubuch <jonas.heubuch@outlook.com>
2025-01-11 21:47:33 +0100

    :lipstick: Fix dialog heights (#471)

--- app/src/main/kotlin/de/hbch/traewelling/ui/composables/ContentDialog.kt
+++ app/src/main/kotlin/de/hbch/traewelling/ui/composables/ContentDialog.kt
@@ -1,10 +1,13 @@
 package de.hbch.traewelling.ui.composables
 
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.heightIn
 import androidx.compose.material3.Card
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalConfiguration
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Dialog
 import androidx.compose.ui.window.DialogProperties
 
@@ -22,7 +25,7 @@ fun ContentDialog(
         )
     ) {
         Card(
-            modifier = modifier.fillMaxWidth()
+            modifier = modifier.fillMaxWidth().heightIn(max = (0.9 * LocalConfiguration.current.screenHeightDp).dp)
         ) {
             content()
         }
