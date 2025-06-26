557f9311f006e93e5417ada6e48a1ce6b8350f15
Marie Ramlow <me@nycode.dev>
2025-06-26 19:23:03 +0200

    app/build.gradle.kts: upgrade com.squareup.retrofit2:converter-gson to 3.0.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -164,7 +164,7 @@ dependencies {
     implementation("androidx.navigation:navigation-compose:2.9.0")
 
     // Retrofit
-    implementation("com.squareup.retrofit2:converter-gson:2.11.0")
+    implementation("com.squareup.retrofit2:converter-gson:3.0.0")
 
     // Coil for image loading
     implementation("io.coil-kt:coil-compose:2.7.0")
