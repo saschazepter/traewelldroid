f12fb85446d11a41fc37ec0219ee943675836589
Marie Ramlow <me@nycode.dev>
2025-06-26 13:03:52 +0200

    upgrade android gradle plugin to 8.11.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
     kotlin("android") version "2.2.0" apply false
-    id("com.android.application") version "8.9.1" apply false
+    id("com.android.application") version "8.11.0" apply false
     id("org.jetbrains.kotlin.plugin.compose") version "2.2.0" apply false
     id("com.google.gms.google-services") version "4.4.2" apply false
     id("com.mikepenz.aboutlibraries.plugin") version "11.1.3" apply false
