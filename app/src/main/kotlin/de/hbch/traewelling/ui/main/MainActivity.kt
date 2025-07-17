b9de9133341b352cf5bfbcc631587d909ca4a75f
Jonas Heubuch <jonas.heubuch@outlook.com>
2025-07-17 17:02:14 +0200

    :sparkles: Add pride notice 🏳️‍🌈 (#521)

--- app/src/main/kotlin/de/hbch/traewelling/ui/main/MainActivity.kt
+++ app/src/main/kotlin/de/hbch/traewelling/ui/main/MainActivity.kt
@@ -9,12 +9,16 @@ import androidx.activity.viewModels
 import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.animation.slideInVertically
 import androidx.compose.animation.slideOutVertically
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.ExperimentalLayoutApi
 import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
@@ -54,6 +58,7 @@ import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import androidx.core.view.WindowCompat
@@ -85,6 +90,7 @@ import de.hbch.traewelling.shared.SettingsViewModel
 import de.hbch.traewelling.shared.SharedValues
 import de.hbch.traewelling.theme.LocalColorScheme
 import de.hbch.traewelling.theme.MainTheme
+import de.hbch.traewelling.ui.composables.ContentDialog
 import de.hbch.traewelling.ui.include.status.ActiveStatusBar
 import de.hbch.traewelling.ui.notifications.NotificationsViewModel
 import de.hbch.traewelling.util.popBackStackAndNavigate
@@ -220,6 +226,7 @@ fun TraewelldroidApp(
     var fabIcon by remember { mutableStateOf<Int?>(null) }
     var fabLabel by remember { mutableStateOf<Int?>(null) }
     var fabListener by remember { mutableStateOf({ }) }
+    var noticeDialogVisible by remember { mutableStateOf(false) }
     var unreadNotificationCount by remember { mutableIntStateOf(0) }
     val notificationsViewModel: NotificationsViewModel = viewModel()
     val onNotificationCountChanged: () -> Unit = {
@@ -316,6 +323,20 @@ fun TraewelldroidApp(
                                     }
                                 }
                             }
+                        } else if (currentDestination?.route == Dashboard::class.qualifiedName) {
+                            Image(
+                                painter = painterResource(id = R.drawable.intersex_inclusive_pride_flag),
+                                contentDescription = null,
+                                modifier = Modifier
+                                    .clickable(
+                                        interactionSource = remember { MutableInteractionSource() },
+                                        indication = null
+                                    ) {
+                                        noticeDialogVisible = true
+                                    }
+                                    .height(24.dp)
+                                    .padding(end = 16.dp)
+                            )
                         }
                     },
                 )
@@ -453,6 +474,35 @@ fun TraewelldroidApp(
                 },
                 onNotificationCountChange = onNotificationCountChanged
             )
+
+            if (noticeDialogVisible) {
+                ContentDialog(
+                    onDismissRequest = {
+                        noticeDialogVisible = false
+                    }
+                ) {
+                    Column(
+                        modifier = Modifier.fillMaxWidth().padding(12.dp),
+                        horizontalAlignment = Alignment.CenterHorizontally,
+                        verticalArrangement = Arrangement.spacedBy(12.dp)
+                    ) {
+                        Image(
+                            painter = painterResource(R.drawable.intersex_inclusive_pride_flag),
+                            contentDescription = null
+                        )
+
+                        Text(
+                            text = "${stringResource(R.string.pride)} \uD83C\uDFF3\uFE0F\u200D\uD83C\uDF08 \uD83C\uDFF3\uFE0F\u200D⚧\uFE0F",
+                            textAlign = TextAlign.Center
+                        )
+
+                        Text(
+                            text = "${stringResource(R.string.contributors_thanks)} ❤",
+                            textAlign = TextAlign.Center
+                        )
+                    }
+                }
+            }
         }
     }
 }
