de063ff4f25d4916d2d3878a7513e70fcde32a7a
Jonas Heubuch <jonas.heubuch@outlook.com>
2025-09-09 15:28:47 +0200

    :sparkles: Display more area information in search (#532)

--- app/src/main/kotlin/de/hbch/traewelling/api/models/station/Station.kt
+++ app/src/main/kotlin/de/hbch/traewelling/api/models/station/Station.kt
@@ -3,10 +3,17 @@ package de.hbch.traewelling.api.models.station
 import com.google.gson.annotations.SerializedName
 
 data class Station(
-    @SerializedName("id") val id: Int,
-    @SerializedName("name") val name: String,
-    @SerializedName("latitude") val latitude: Double,
-    @SerializedName("longitude") val longitude: Double,
+    val id: Int,
+    val name: String,
+    val latitude: Double,
+    val longitude: Double,
     @SerializedName("ibnr") val evaIdentifier: Long?,
-    @SerializedName("rilIdentifier") val ds100: String?
+    @SerializedName("rilIdentifier") val ds100: String?,
+    val areas: List<Area>
+)
+
+data class Area(
+    val name: String,
+    val adminLevel: Int,
+    val default: Boolean
 )
