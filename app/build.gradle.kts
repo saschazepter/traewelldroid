8d9acbe6afb0e0399b5609726ef3e6fcdce1833d
Jonas Heubuch <jonas.heubuch@outlook.com>
2025-09-22 10:15:51 +0200

    :bookmark: Bump version to 2.21.3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,8 +18,8 @@ aboutLibraries {
 
 android {
     val packageName = "de.hbch.traewelling"
-    val name = "2.21.2"
-    val code = 203
+    val name = "2.21.3"
+    val code = 204
     compileSdk = 36
 
     dependenciesInfo {
