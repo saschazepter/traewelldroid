e6866fbca36b4b1f0586a632640ae58b23a442bc
Jonas Heubuch <jonas@hbch.de>
2026-04-02 10:50:57 +0200

    :construction_worker: Generate release notes in pipeline (#591)
    Reviewed-on: https://codeberg.org/traewelldroid/traewelldroid/pulls/591
    Co-authored-by: Jonas Heubuch <jonas@hbch.de>
    Co-committed-by: Jonas Heubuch <jonas@hbch.de>

---
+++ release_notes.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+OUTPUT_FILE="${RUNNER_TEMP:-.}/RELEASE_NOTES.md"
+
+LATEST_TAG=$(git describe --tags --abbrev=0 2>/dev/null)
+
+if [ -z "$LATEST_TAG" ]; then
+  echo "Error: Could not find any tag"
+  exit 1
+fi
+
+PREVIOUS_TAG=$(git describe --tags --abbrev=0 "${LATEST_TAG}^" 2>/dev/null)
+
+echo "# What's changed in ${LATEST_TAG}" > "$OUTPUT_FILE"
+
+echo "" >> "$OUTPUT_FILE"
+
+if [ -z "$PREVIOUS_TAG" ]; then
+  git log "$LATEST_TAG" --reverse --pretty=format:"- %s by %an" >> "$OUTPUT_FILE"
+else
+  git log "${PREVIOUS_TAG}..${LATEST_TAG}" --reverse --pretty=format:"- %s by %an" >> "$OUTPUT_FILE"
+fi
+
+echo "" >> "$OUTPUT_FILE"
+
+echo "Release Notes created."
