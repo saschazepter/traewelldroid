8a7b058a30614e3c86b5718a6a6dfa85540f8156
Jonas H. <jonas@hbch.de>
2026-07-04 20:39:37 +0200

    :bookmark: Bump version to 2.26.3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,8 +16,8 @@ aboutLibraries {
 
 android {
     val packageName = "de.hbch.traewelling"
-    val name = "2.26.2"
-    val code = 226
+    val name = "2.26.3"
+    val code = 227
     compileSdk = 37
 
     dependenciesInfo {
